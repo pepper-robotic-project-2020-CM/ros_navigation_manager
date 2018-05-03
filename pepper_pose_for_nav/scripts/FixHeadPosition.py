70d2e8c03dc8d2ee53aad0e1fc8410eb81576a1f
jsaraydaryan <jacques.saraydaryan@cpe.fr>
2018-05-03 14:41:57 +0200

    add service to fix pepper head on demand

--- pepper_pose_for_nav/scripts/FixHeadPosition.py
+++ pepper_pose_for_nav/scripts/FixHeadPosition.py
@@ -5,40 +5,51 @@ import argparse
 import sys
 import time
 import rospy
+from pepper_pose_for_nav.srv import FixHeadAtPosition
+
+class HeadFix():
 
 
-def HeadFix(session):
-    """
-    This example uses the setExternalCollisionProtectionEnabled method.
-    """
-    # Get the service ALMotion.
-    motion_service  = session.service("ALMotion")
-    memory_service = session.service("ALMemory")
-    autolife_service = session.service("ALAutonomousLife")
-    autolife_service.setState('disabled')
-    posture_service = session.service("ALRobotPosture")
-    posture_service.goToPosture("Stand",0.3)
+    def __init__(self,session):
+        """
+        This example uses the setExternalCollisionProtectionEnabled method.
+        """
+        # Get the service ALMotion.
+        self._motion_service  = session.service("ALMotion")
+        self._memory_service = session.service("ALMemory")
+        self._autolife_service = session.service("ALAutonomousLife")
+        self._autolife_service.setState('disabled')
+        self._posture_service = session.service("ALRobotPosture")
+        self._posture_service.goToPosture("Stand",0.3)
 
 
-    fractionMaxSpeed = 0.5
-    motion_service.setStiffnesses("HEAD", 1.0)
-    motion_service.setStiffnesses("TORSO", 1.0)
-    error=0.1
-    #pitch_value=0.3
-    pitch_value=0.0
-    isEnd=False
+        self._fractionMaxSpeed = 0.2
+        self._motion_service.setStiffnesses("HEAD", 1.0)
+        self._motion_service.setStiffnesses("TORSO", 1.0)
+        self._error=0.1
+        #pitch_value=0.3
+        self._pitch_value=0.0
+        self._isEnd=False
+        #declare ros service 
+        self.setHeadPositionSrv = rospy.Service('fix_head_pose_srv', FixHeadAtPosition, self.setHeadPositionSrvCallback)
 
-    while not rospy.is_shutdown():
-            headYawPos = memory_service.getData("Device/SubDeviceList/HeadYaw/Position/Sensor/Value")
-            headPitchPos = memory_service.getData("Device/SubDeviceList/HeadPitch/Position/Sensor/Value")
+
+    def fixHead(self):
+        while not rospy.is_shutdown():
+            headYawPos = self._memory_service.getData("Device/SubDeviceList/HeadYaw/Position/Sensor/Value")
+            headPitchPos = self._memory_service.getData("Device/SubDeviceList/HeadPitch/Position/Sensor/Value")
             print("headYawPos:"+str(headYawPos)+",headPitchPos:"+str(headPitchPos))
             #if abs(headPitchPos)>error or abs(headYawPos)>error:
-            if headPitchPos>(pitch_value+error) or headPitchPos<(pitch_value-error)  or abs(headYawPos)>error:
-                motion_service.setAngles("HeadYaw", 0.0, fractionMaxSpeed) 
-                motion_service.setAngles("HeadPitch", pitch_value, fractionMaxSpeed) ## fix head on the horizon 0.0, fix head looking for obstacle 0.3
+            if headPitchPos>(self._pitch_value+self._error) or headPitchPos<(self._pitch_value-self._error)  or abs(headYawPos)>self._error:
+                self._motion_service.setAngles("HeadYaw", 0.0, self._fractionMaxSpeed) 
+                self._motion_service.setAngles("HeadPitch", self._pitch_value, self._fractionMaxSpeed) ## fix head on the horizon 0.0, fix head looking for obstacle 0.3
                 print("update head")
 
             time.sleep(0.1)
+
+    def setHeadPositionSrvCallback(self,req):
+        self._pitch_value=req.pitch_value
+        return True
   
 
 if __name__ == "__main__":
@@ -53,5 +64,10 @@ if __name__ == "__main__":
         print ("Can't connect to Naoqi at ip \"" + ip + "\" on port " + str(port) +".\n"
                "Please check your script arguments. Run with -h option for help.")
         sys.exit(1)
-    HeadFix(session)
+    headFix=HeadFix(session)
+    headFix.fixHead()
+    # spin
+    rospy.spin()
+
+
 
