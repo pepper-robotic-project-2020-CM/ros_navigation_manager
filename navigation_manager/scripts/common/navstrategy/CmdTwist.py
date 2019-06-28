5bfae1914b142cd739de8179ae9daf95356b7ea7
Xia0ben <10845530+Xia0ben@users.noreply.github.com>
2019-06-28 09:29:24 +0200

    Add salle thesard map, improve MoveHeadPosition to possibly reactivate Head Basic Awareness to look at human

--- navigation_manager/scripts/common/navstrategy/CmdTwist.py
+++ navigation_manager/scripts/common/navstrategy/CmdTwist.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import time
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Vector3
+
 
 class CmdTwist(object):
-    _cmdMsg=''
-    _duration=0
-    _startTime=0 # in second
-    _stopTime=0 # in second
+    _cmdMsg = ''
+    _duration = 0
+    _startTime = 0  # in second
+    _stopTime = 0  # in second
 
-
-    def __init__(self,cmdMsg):
-        self._cmdMsg=cmdMsg
-        self._startTime=time.time()
+    def __init__(self, cmdMsg):
+        self._cmdMsg = cmdMsg
+        self._startTime = time.time()
     
-    def setStopTime(self,stopTime):
-        self._stopTime=stopTime
-        self._duration=stopTime-self._startTime
+    def setStopTime(self, stopTime):
+        self._stopTime = stopTime
+        self._duration = stopTime - self._startTime
 
     def duration(self):
        return self._duration
@@ -25,14 +25,7 @@ class CmdTwist(object):
         return self._cmdMsg
 
     def reverse(self):
-        r_cmd=Twist()
-        r_cmd.linear.x=self._cmdMsg.linear.x * -1
-        r_cmd.linear.y=self._cmdMsg.linear.y * -1
-        r_cmd.linear.z=self._cmdMsg.linear.z * -1
-        r_cmd.angular.x=self._cmdMsg.angular.x * -1
-        r_cmd.angular.y=self._cmdMsg.angular.y * -1
-        r_cmd.angular.z=self._cmdMsg.angular.z * -1
-        return r_cmd
-
-
-    
+        return Twist(
+            linear=Vector3(x=self._cmdMsg.linear.x * -1, y=self._cmdMsg.linear.y * -1, z=self._cmdMsg.linear.z * -1),
+            angular=Vector3(x=self._cmdMsg.angular.x * -1, y=self._cmdMsg.angular.y * -1, z=self._cmdMsg.angular.z * -1)
+        )
