7cf71cf21ed9db75dbf085a498580f9df0469a58
koalp <koalp@alpaga.dev>
2020-01-13 18:05:17 +0100

    fix: add init file in goal module

---
+++ navigation_manager/scripts/common/goal/goal.py
@@ -0,0 +1,26 @@
+""" tools to manipulate goal poses """
+
+
+class GoalPose():
+    """ pose of goal """
+
+    source = None
+    target = None
+    goal_type = ''
+
+    def __init__(self, source=None, target=None, goal_type=''):
+        """ creates a goal pose
+
+        :param target: target pose
+        :type target: Pose
+        :param source: source pose
+        :type source: Pose
+        """
+
+        self.source = source
+        self.target = target
+        self.goal_type = goal_type
+
+    def parameters(self):
+        """ return parameters in order to launch funtions """
+        return self.source, self.goal_type, self.target
